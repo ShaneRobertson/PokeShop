8397e002b83efa3e709e0211a537a9b490455b17
Shane Robertson <sjrobertson0812@gmail.com>
2021-12-23 13:20:54 -0500

    server is working

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const server = express();
-
+const apiRouter = require("./routes");
 const morgan = require("morgan");
+
 server.use(morgan("dev"));
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+
+server.use("/users", apiRouter);
 
 const path = require("path");
-server.use("/static", express.static(path.join(__dirname, "/public")));
+server.use(express.static(path.join(__dirname, "public")));
 
 server.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "/public", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
   console.log(`server is listening on port: ${PORT}`);
-  console.log(__dirname);
 });
