cf20d3eb5f4a9f53473ac5b8303dca4b02b7bb63
Shane Robertson <sjrobertson0812@gmail.com>
2022-01-04 06:14:35 -0500

    committing so I can start switching over to ES6 modules- import/export

--- public/login/login.js
+++ public/login/login.js
@@ -1,13 +1,16 @@
+const userLogin = require("../../api");
 const usernameEl = document.getElementById("username");
 const passwordEl = document.getElementById("password");
 const loginBtn = document.getElementById("loginBtn");
 
 loginBtn.addEventListener("click", async (e) => {
   e.preventDefault();
+  console.log("hello");
   const username = usernameEl.value;
   const password = passwordEl.value;
 
-  console.log(username, password);
+  const returnedVal = await userLogin(username, password);
+  console.log("returned Value in front end: ", returnedVal);
 
   usernameEl.value = "";
   passwordEl.value = "";
