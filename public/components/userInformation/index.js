29008a1b5f10af85a23d3192610ed46d1d6f8b90
Shane Robertson <sjrobertson0812@gmail.com>
2022-03-05 14:24:51 -0500

    staging these changes for now

--- public/components/userInformation/index.js
+++ public/components/userInformation/index.js
@@ -1,38 +1,23 @@
+const usersEmail = document.getElementById("user-details-email");
+const usersUsername = document.getElementById("user-details-username");
+
 const getUserInfo = async () => {
+  console.log("running getUserInfo");
   try {
-    if (localStorage.getItem("token")) {
-      let token = localStorage.getItem("token");
-      console.log("token type: ", typeof token);
-      const response = await fetch("/users", {
-        method: "POST",
-        headers: {
-          authorization: `Bearer ${token}`,
-        },
-      });
-      const userInformation = await response.json();
-      console.log(userInformation);
-    }
+    // if (localStorage.getItem("token"))
+    let token = JSON.parse(localStorage.getItem("token"));
+    const response = await fetch("/api/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${token}`,
+      },
+    });
+    const userInformation = response.json();
+    console.log("userinfo: ", userInformation);
   } catch (error) {
     console.log(error);
   }
 };
-
-// const loginUser = async (username, userPassword) => {
-//     let userObj = { username, userPassword };
-//     try {
-//       const response = await fetch("/login", {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify(userObj),
-//       });
-//       const result = await response.json();
-//       return result;
-//     } catch (err) {
-//       console.log("asdfasdfas");
-//       console.log(err);
-//     }
-//   };
 
 getUserInfo();
